32b2373e7fd5a3abb7de8daf9bed0a4b8b9a7640
user <2511829475@qq.com>
2022-09-08 09:33:58 +0800

    push1111

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("push了一句话");
+        System.out.println("123");
+        System.out.println("-----");
     }
 }
